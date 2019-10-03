caa904e710f49fe74d8d9a5efec1967664791dcf
Mike Lewis <mike@mplew.is>
2019-10-03 19:10:45 -0400

    Silence hook output

--- scripts/hooks/post-checkout.sh
+++ scripts/hooks/post-checkout.sh
@@ -2,4 +2,4 @@
 
 # Re-sync constraints file using Makefile in constraints directory
 # Hook runs from project's top-level directory, so paths are relative to that
-make --directory=source/constraints clean constraints
+make --directory=source/constraints clean constraints 1>/dev/null 2>&1
