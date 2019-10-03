7c08c9ac84dfe867823d43da681b338a396789b5
Mike Lewis <mike@mplew.is>
2019-10-03 18:13:31 -0400

    Add hook installation to keep constraints file in sync after reset

---
+++ scripts/hooks/post-checkout.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Re-sync constraints file using Makefile in constraints directory
+# Hook runs from project's top-level directory, so paths are relative to that
+make --directory=source/constraints clean constraints
